c894f760343622053705d06811545dda4bcbefcc
Brendan Nolan <bnolan23@gmail.com>
2025-10-16 10:54:35 +0100

    Get ready to time the multiplications

--- tests/test_a.cpp
+++ tests/test_a.cpp
@@ -1,9 +1,15 @@
 #include "matrix.hpp"
+
+#include <chrono>
+#include <iostream>
+
 #include <gtest/gtest.h>
 
 TEST(FirstTest, MultiplyDim) {
     const auto dim = lin_alg::Dimension{10000, 10000};
     const auto a = lin_alg::Matrix::random(dim);
     const auto b = lin_alg::Matrix::random(dim);
-    EXPECT_EQ(lin_alg::naive_multiply(a, b), tiled_multiply(a, b, 2));
+    const auto tiled_multiply_result = lin_alg::tiled_multiply(a, b, 4U);
+    const auto naive_multiply_result = lin_alg::naive_multiply(a, b);
+    EXPECT_EQ(tiled_multiply_result, naive_multiply_result);
 }

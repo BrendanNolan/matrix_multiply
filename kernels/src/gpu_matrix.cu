91c95ff6a80979db8b8961175ae77f5b6a1c71eb
Brendan Nolan <bnolan23@gmail.com>
2025-10-27 14:37:02 +0000

    Add some cuda code

--- kernels/src/gpu_matrix.cu
+++ kernels/src/gpu_matrix.cu
@@ -1,1 +1,12 @@
 #include "gpu_matrix.cuh"
+
+__host__ __device__ size_t total(const Pos2& pos) {
+    return pos.i * pos.j;
+}
+
+__global__ void tiled_multiply(const float* A,
+        const Pos2& size_A,
+        const float* B,
+        const Pos2& size_B,
+        float* C) {
+}

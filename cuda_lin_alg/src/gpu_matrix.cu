52890f7d896f6a16659279f6afa79f8761517b63
Brendan Nolan <bnolan23@gmail.com>
2025-11-01 17:58:28 +0000

    Try to improve

--- cuda_lin_alg/src/gpu_matrix.cu
+++ cuda_lin_alg/src/gpu_matrix.cu
@@ -20,16 +20,16 @@ __global__ void tiled_multiply(const float* A,
     float* c_tile = shared + 2 * T * T;
     const auto g_i = blockIdx.x * blockDim.x + threadIdx.x;
     const auto g_j = blockIdx.y * blockDim.y + threadIdx.y;
-    const auto g_c_cell = g_i * bj + g_j;
-    if (g_c_cell >= ai * bj)
+    if (g_i >= ai || g_j >= bj)
         return;
+    const auto g_c_cell = g_i * bj + g_j;
     const auto l_c_cell = threadIdx.x * T + threadIdx.y;
     c_tile[l_c_cell] = 0.0f;
     for (auto k = 0U; k < aj; k += T) {
         a_tile[l_c_cell] = A[g_i * aj + (k + threadIdx.y)];
         b_tile[l_c_cell] = B[(k + threadIdx.x) * bj + g_j];
         __syncthreads();
-        for (auto kk = k; kk < min(aj, static_cast<size_t>(k + T)); ++kk) {
+        for (auto kk = 0; kk + k < min(k + T, aj); ++kk) {
             c_tile[l_c_cell] += a_tile[threadIdx.x * T + kk] * b_tile[kk * T + threadIdx.y];
         }
     }

954af36cdabe01c4c8b4eb459e42e4ff685e927b
Brendan Nolan <bnolan23@gmail.com>
2025-11-02 14:47:21 +0000

    Extend tests

--- tests/cpu_vs_cputiled_vs_gputiled.cu
+++ tests/cpu_vs_cputiled_vs_gputiled.cu
@@ -7,12 +7,14 @@
 
 #include <gtest/gtest.h>
 
+namespace {
+
 using Dim = lin_alg::Dimension;
 
-namespace {
 lin_alg::Matrix cuda_tiled_multiply(const lin_alg::Matrix& a,
         const lin_alg::Matrix& b,
-        unsigned int tile_size) {
+        const unsigned int tile_size,
+        const bool print_timing) {
     const auto a_bytes = raw_size(a) * sizeof(float);
     float* A;
     cudaMalloc(&A, a_bytes);
@@ -36,20 +38,26 @@ lin_alg::Matrix cuda_tiled_multiply(const lin_alg::Matrix& a,
     const auto end = std::chrono::high_resolution_clock::now();
     const auto duration_ms =
             std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    std::cout << "Optimised GPU execution time: " << duration_ms << " ms" << std::endl;
+    if (print_timing)
+        std::cout << "Optimised GPU execution time: " << duration_ms << " ms" << std::endl;
     float* h_C = static_cast<float*>(malloc(c_bytes * sizeof(float)));
     cudaMemcpy(h_C, C, c_bytes, cudaMemcpyDeviceToHost);
     return lin_alg::Matrix::from_raw(h_C, lin_alg::Dimension{a.dim().i, b.dim().j});
 }
 
-}// namespace
-
-TEST(FirstTest, MultiplyDim) {
-    const auto rows_left = (1U << 11U) + 1U;
-    const auto common = (1U << 10U) + 3U;
-    const auto columns_right = (1U << 12U) + 1U;
+void correctness_test(const unsigned int rows_left,
+        const unsigned int common,
+        const unsigned int columns_right) {
     const auto a = lin_alg::Matrix::random(Dim{rows_left, common});
     const auto b = lin_alg::Matrix::random(Dim{common, columns_right});
+    const auto naive_multiply_result = lin_alg::naive_multiply(a, b);
+    const auto cuda_multiply_result = cuda_tiled_multiply(a, b, 4U, false);
+    EXPECT_EQ(cuda_multiply_result, naive_multiply_result);
+}
+
+void speed_test(const unsigned int dim_of_square_matrix) {
+    const auto a = lin_alg::Matrix::random(Dim{dim_of_square_matrix, dim_of_square_matrix});
+    const auto b = lin_alg::Matrix::random(Dim{dim_of_square_matrix, dim_of_square_matrix});
 
     auto start = std::chrono::high_resolution_clock::now();
     const auto naive_multiply_result = lin_alg::naive_multiply(a, b);
@@ -65,9 +73,32 @@ TEST(FirstTest, MultiplyDim) {
             std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     std::cout << "Optimised CPU execution time: " << tiled_time << " ms" << std::endl;
 
-    // This function will print its own timing
-    const auto cuda_multiply_result = cuda_tiled_multiply(a, b, 4U);
+    const auto cuda_multiply_result = cuda_tiled_multiply(a, b, 4U, true);
 
     EXPECT_EQ(tiled_multiply_result, naive_multiply_result);
     EXPECT_EQ(cuda_multiply_result, naive_multiply_result);
+}
+
+}// namespace
+
+TEST(SpeedTest, OneMillionElements) {
+    speed_test(1U << 10U);
+}
+
+TEST(SpeedTest, OneThousandElements) {
+    speed_test(1U << 7U);
+}
+
+TEST(CorrectnessTest, Small) {
+    const auto rows_left = (1U << 5) + 1U;
+    const auto common = (1U << 4) + 3U;
+    const auto columns_right = (1U << 6) + 1U;
+    correctness_test(rows_left, common, columns_right);
+}
+
+TEST(CorrectnessTest, Large) {
+    const auto rows_left = (1U << 8) + 1U;
+    const auto common = (1U << 7) + 3U;
+    const auto columns_right = (1U << 6) + 1U;
+    correctness_test(rows_left, common, columns_right);
 }

80853cc34b616d34dcc7a32f1c1edd7973467821
Brendan Nolan <bnolan23@gmail.com>
2025-11-04 08:50:47 +0000

    Add grid-stride to CUDA matrix multiplication

--- cuda_lin_alg/src/gpu_matrix.cu
+++ cuda_lin_alg/src/gpu_matrix.cu
@@ -14,23 +14,27 @@ __global__ void tiled_multiply(const float* A,
     float* a_tile = shared;
     float* b_tile = shared + T * T;
     float* c_tile = shared + 2 * T * T;
-    const auto g_i = blockIdx.x * blockDim.x + threadIdx.x;
-    const auto g_j = blockIdx.y * blockDim.y + threadIdx.y;
-    const auto l_c_cell = threadIdx.x * T + threadIdx.y;
-    c_tile[l_c_cell] = 0.0f;
-    for (auto k = 0U; k < aj; k += T) {
-        const auto in_scope_for_a = (g_i < ai && k + threadIdx.y < aj);
-        const auto in_scope_for_b = (k + threadIdx.x < aj && g_j < bj);
-        a_tile[l_c_cell] = in_scope_for_a ? A[g_i * aj + (k + threadIdx.y)] : 0U;
-        b_tile[l_c_cell] = in_scope_for_b ? B[(k + threadIdx.x) * bj + g_j] : 0U;
-        __syncthreads();
-        for (auto kk = 0U; kk < T; ++kk) {
-            c_tile[l_c_cell] += a_tile[threadIdx.x * T + kk] * b_tile[kk * T + threadIdx.y];
+    for (auto x = 0U; x < ai; x += gridDim.x * blockDim.x) {
+        for (auto y = 0U; y < bj; y += gridDim.y * blockDim.y) {
+            const auto g_i = x + blockIdx.x * blockDim.x + threadIdx.x;
+            const auto g_j = y + blockIdx.y * blockDim.y + threadIdx.y;
+            const auto l_c_cell = threadIdx.x * T + threadIdx.y;
+            c_tile[l_c_cell] = 0.0f;
+            for (auto k = 0U; k < aj; k += T) {
+                const auto in_scope_for_a = (g_i < ai && k + threadIdx.y < aj);
+                const auto in_scope_for_b = (k + threadIdx.x < aj && g_j < bj);
+                a_tile[l_c_cell] = in_scope_for_a ? A[g_i * aj + (k + threadIdx.y)] : 0U;
+                b_tile[l_c_cell] = in_scope_for_b ? B[(k + threadIdx.x) * bj + g_j] : 0U;
+                __syncthreads();
+                for (auto kk = 0U; kk < T; ++kk) {
+                    c_tile[l_c_cell] += a_tile[threadIdx.x * T + kk] * b_tile[kk * T + threadIdx.y];
+                }
+                __syncthreads();
+            }
+            if (g_i < ai && g_j < bj)
+                C[g_i * bj + g_j] = c_tile[l_c_cell];
         }
-        __syncthreads();
     }
-    if (g_i < ai && g_j < bj)
-        C[g_i * bj + g_j] = c_tile[l_c_cell];
 }
 
 }// namespace cuda_lin_alg
